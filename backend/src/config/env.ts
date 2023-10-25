d0acd1cbfe1fa6683baa561ae17227a0e4e48060
Nizom Sidiq <nizomsidiq@gmail.com>
2023-10-25 16:29:24 +0700

    backend: remove unused env variable

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -14,7 +14,6 @@ const env = {
   HOST: process.env.HOST || '127.0.0.1',
   CORS_ORIGIN: process.env.CORS_ORIGIN || (environment === 'development' ? '*' : undefined),
   MONGO_URI: process.env.MONGO_URI,
-  REDIS_URI: process.env.REDIS_URI,
   JWT_SECRET: process.env.JWT_SECRET,
   SOCKETIO_ADAPTER: process.env.SOCKETIO_ADAPTER || 'memory',
   SOCKETIO_REDIS_URI: process.env.SOCKETIO_REDIS_URI || undefined,
@@ -26,7 +25,6 @@ const ok = v.compile({
   HOST: { type: 'string', empty: false, required: true },
   CORS_ORIGIN: { type: 'string', required: true },
   MONGO_URI: { type: 'string', required: true },
-  REDIS_URI: { type: 'string', required: true },
   JWT_SECRET: { type: 'string', required: true },
   SOCKETIO_ADAPTER: { type: 'enum', required: true, values: ['memory', 'redis'] },
   SOCKETIO_REDIS_URI: { type: 'string', required: false, optional: true },
@@ -37,7 +35,6 @@ export default env as unknown as {
   HOST: string
   CORS_ORIGIN: string
   MONGO_URI: string
-  REDIS_URI: string
   JWT_SECRET: string
   SOCKETIO_ADAPTER: 'redis' | 'memory'
   SOCKETIO_REDIS_URI: string | undefined
